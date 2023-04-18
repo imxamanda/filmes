85498db9f9c8c039a76715b22dec311ae3f83ce6
imxamanda <amandavieiraperes.12@gmail.com>
2023-04-17 22:13:27 -0300

    without api

--- services/apiFilmes.js
+++ services/apiFilmes.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjE2M2NhZjQ4NjNkODY5Y2ZjZjQ5ZGJjODM4MjA5NyIsInN1YiI6IjY0MzQ4ZTFhZWM0NTUyMDBiNTgxNDUyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vT75flEFTAfzTZEtRhx5z12LBVHtmfFiD50zF6jzhxs'//chave dentro das aspas Token de Leitura da API (v4 auth)
+        Authorization: 'Bearer ' + process.env.API_KEY_TMDB   //chave dentro das aspas Token de Leitura da API (v4 auth)
     }
 })
 
