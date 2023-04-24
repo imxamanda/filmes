d854f7d616d165e762abddc1bf5b4d23b760e517
imxamandav <amandavieiraperes.12@gmail.com>
2023-04-24 11:58:48 -0300

    adição de atores

---
+++ pages/atores/[id].js
@@ -0,0 +1,79 @@
+import Navbar from '@/components/Navbar'
+import apiFilmes from '@/services/apiFilmes'
+import React from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
+
+const Detalhes = ({ ator, imagens, filmes, series }) => {
+    return (
+
+        <Navbar titulo={ator.name}>
+
+            <Row>
+                <Col md={3}>
+                    <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500/' + ator.profile_path} />
+                </Col>
+
+                <Col md={9}>
+                    <p><strong>🗓 Data de Nascimento: </strong>{ator.birthday}</p>
+                    <p><strong>📌 Cidade Natal: </strong>{ator.place_of_birth}</p>
+                    <p><strong>📖 Biografia: </strong>{ator.biography}</p>
+                </Col>
+            </Row>
+
+            <h2 className='mt-4'>Imagens</h2>
+            <Row>
+                {imagens.map(item => (
+                    <Col className='mb-4' md={1}>
+                        <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500/' + item.file_path} />
+                    </Col>
+                ))}
+            </Row>
+
+            <h2 className='mt-4'>Filmografia:</h2>
+            <Row>
+                {filmes.map(item => (
+                    <Col className='mb-4' md={2} title={item.name}>
+                        <Link href={'/filmes/' + item.id}>
+                            <Card.Img title={item.name} variant="top" src={'https://image.tmdb.org/t/p/w500/' + item.poster_path} />
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+
+            <h2 className='mt-4'>Produções de TV que participou:</h2>
+            <Row>
+                {series.map(item => (
+                    <Col className='mb-4' md={2} title={item.name}>
+                        <Link href={'/filmes/' + item.id}>
+                            <Card.Img title={item.name} variant="top" src={'https://image.tmdb.org/t/p/w500/' + item.poster_path} />
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+
+        </Navbar>
+    )
+}
+
+export default Detalhes
+
+export async function getServerSideProps(context) {
+
+    const id = context.params.id
+
+    const resultado = await apiFilmes.get('/person/' + id + '?language=pt-BR')
+    const ator = resultado.data
+
+    const resImages = await apiFilmes.get('/person/' + id + '/images?language=pt-BR')
+    const imagens = resImages.data.profiles
+
+    const resFilmes = await apiFilmes.get('/person/' + id + '/movie_credits?language=pt-BR')
+    const filmes = resFilmes.data.cast
+
+    const resTv = await apiFilmes.get('/person/' + id + '/tv_credits?language=pt-BR')
+    const tvs = resTv.data.cast
+
+    return {
+        props: { ator, imagens, filmes, series }
+    }
+}
